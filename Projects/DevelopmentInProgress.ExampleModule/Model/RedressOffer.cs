c01dff593d889b712cb01ad9e74265e60fd24f14
grantcolley <grantcolley@yahoo.com>
2015-05-24 22:11:14 +0100

    DipState example wip
    DipState example wip

--- Projects/DevelopmentInProgress.ExampleModule/Model/RedressOffer.cs
+++ Projects/DevelopmentInProgress.ExampleModule/Model/RedressOffer.cs
@@ -2,16 +2,31 @@
 
 namespace DevelopmentInProgress.ExampleModule.Model
 {
-    public class RedressOffer
+    public class CustomerRemediation
     {
+        // Client Communication
+        public DateTime? LetterSent { get; set; }
+        public DateTime? ResponseReceived { get; set; }
+        public decimal? ConsequentialLossClaim { get; set; }
+
+        // Data capture
         public string HedgingProduct { get; set; }
-        public decimal? TotalPaymentAmount { get; set; }
-        public decimal? ConsequentialLoss { get; set; }
+        public decimal? NominalAmount { get; set; }
         public decimal? Interest { get; set; }
         public DateTime? StartDate { get; set; }
-        public decimal? TotalRefund { get; set; }
-        public bool OffsetApplicable { get; set; }
+        public decimal? RedressRate { get; set; }
+        public decimal? RedressAmount { get; set; }
+
+        // Offset Applicable
+        public bool? OffsetApplicable { get; set; }
+
+        // Apply Offset
         public decimal? OffsetAmount { get; set; }
-        public decimal? FinalOffer { get; set; }
+
+        // Redress Review
+        public decimal? FinalRedressAmount { get; set; }
+
+        // Payment Executed
+        public DateTime? PaymentExecuted { get; set; }
     }
 }
