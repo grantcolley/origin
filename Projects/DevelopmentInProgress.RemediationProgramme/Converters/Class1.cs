c10e8ec73d84da21bdd5989d67bc1dac6a1583cc
grantcolley <grantcolley@yahoo.com>
2015-06-09 23:45:56 +0100

    Add arrow to states
    Add arrow to states

---
+++ Projects/DevelopmentInProgress.RemediationProgramme/Converters/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using DevelopmentInProgress.DipState;
+
+namespace DevelopmentInProgress.RemediationProgramme.Converters
+{
+    public class InvertedDipStateStatusToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            if (value != null
+                && ((DipStateStatus)value).Equals(DipStateStatus.Completed))
+            {
+                return Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
