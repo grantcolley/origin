338f735ada065b5d40341632d97eecdbbea6f1c9
grantcolley <grantcolley@yahoo.com>
2017-11-12 09:39:16 +0000

    Add more test data
    Add more test data

--- Projects/DevelopmentInProgress.ExampleModule/Service/Data.cs
+++ Projects/DevelopmentInProgress.ExampleModule/Service/Data.cs
@@ -40,7 +40,79 @@ namespace DevelopmentInProgress.ExampleModule.Service
                 AccountNumber = "11779988",
                 Address = "951 Cambridge Road, Hampstead, London, NW2 7HA"
             });
-            
+
+            customers.Add(new Customer()
+            {
+                Name = "John Maynard Keynes",
+                SortCode = "12-34-56",
+                AccountNumber = "11223344",
+                Address = "123 Main Street, Hatfield, London, SW1 2AB"
+            });
+
+            customers.Add(new Customer()
+            {
+                Name = "Adam Smith",
+                SortCode = "12-56-78",
+                AccountNumber = "11556677",
+                Address = "987 High Street, Oxford, London, RA1 2QA"
+            });
+
+            customers.Add(new Customer()
+            {
+                Name = "Merrill Flood",
+                SortCode = "12-98-32",
+                AccountNumber = "11997788",
+                Address = "456 Garden Path, Canary Wharf, London, CW1 1WP"
+            });
+
+            customers.Add(new Customer()
+            {
+                Name = "Melvin Dresher",
+                SortCode = "12-34-45",
+                AccountNumber = "11779988",
+                Address = "951 Cambridge Road, Hampstead, London, NW2 7HA"
+            });
+
+            customers.Add(new Customer()
+            {
+                Name = "John Nash",
+                SortCode = "12-34-56",
+                AccountNumber = "11223344",
+                Address = "123 Main Street, Hatfield, London, SW1 2AB"
+            });
+
+            customers.Add(new Customer()
+            {
+                Name = "Apple Crunch",
+                SortCode = "12-56-78",
+                AccountNumber = "11556677",
+                Address = "987 High Street, Oxford, London, RA1 2QA"
+            });
+
+            customers.Add(new Customer()
+            {
+                Name = "Maple Syrup",
+                SortCode = "12-98-32",
+                AccountNumber = "11997788",
+                Address = "456 Garden Path, Canary Wharf, London, CW1 1WP"
+            });
+
+            customers.Add(new Customer()
+            {
+                Name = "Sticky Toffee",
+                SortCode = "12-34-45",
+                AccountNumber = "11779988",
+                Address = "951 Cambridge Road, Hampstead, London, NW2 7HA"
+            });
+
+            customers.Add(new Customer()
+            {
+                Name = "Eton Mess",
+                SortCode = "12-34-56",
+                AccountNumber = "11223344",
+                Address = "123 Main Street, Hatfield, London, SW1 2AB"
+            });
+
             return customers;
         }
     }
